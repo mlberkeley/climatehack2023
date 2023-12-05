5f7cd9e136e5c0d2f504fe72533245a253bdd369
Grace Tang <grace.tang@berkeley.edu>
2023-12-05 03:14:48 +0000

    Copy things over from getting-started

---
+++ scripts/challenge_dataset.py
@@ -0,0 +1,59 @@
+from torch.utils.data import IterableDataset
+from datetime import datetime, time, timedelta
+class ChallengeDataset(IterableDataset):
+    def __init__(self, pv, hrv, site_locations, sites=None):
+        self.pv = pv
+        self.hrv = hrv
+        self._site_locations = site_locations
+        self._sites = sites if sites else list(site_locations["hrv"].keys())
+
+    def _get_image_times(self):
+        min_date = datetime(2020, 1, 1)
+        max_date = datetime(2020, 7, 30)
+
+        start_time = time(8)
+        end_time = time(17)
+
+        date = min_date
+        while date <= max_date:
+            current_time = datetime.combine(date, start_time)
+            while current_time.time() < end_time:
+                if current_time:
+                    yield current_time
+
+                current_time += timedelta(minutes=60)
+
+            date += timedelta(days=1)
+
+    def __iter__(self):
+        for time in self._get_image_times():
+            first_hour = slice(str(time), str(time + timedelta(minutes=55)))
+
+            pv_features = pv.xs(first_hour, drop_level=False)  # type: ignore
+            pv_targets = pv.xs(
+                slice(  # type: ignore
+                    str(time + timedelta(hours=1)),
+                    str(time + timedelta(hours=4, minutes=55)),
+                ),
+                drop_level=False,
+            )
+
+            hrv_data = self.hrv["data"].sel(time=first_hour).to_numpy()
+
+            for site in self._sites:
+                try:
+                    # Get solar PV features and targets
+                    site_features = pv_features.xs(site, level=1).to_numpy().squeeze(-1)
+                    site_targets = pv_targets.xs(site, level=1).to_numpy().squeeze(-1)
+                    assert site_features.shape == (12,) and site_targets.shape == (48,)
+
+                    # Get a 128x128 HRV crop centred on the site over the previous hour
+                    x, y = self._site_locations["hrv"][site]
+                    hrv_features = hrv_data[:, y - 64 : y + 64, x - 64 : x + 64, 0]
+                    assert hrv_features.shape == (12, 128, 128)
+
+                    # How might you adapt this for the non-HRV, weather and aerosol data?
+                except:
+                    continue
+
+                yield site_features, hrv_features, site_targets
