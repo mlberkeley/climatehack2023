c448f335adc3d2c51264fbf0972bb685e58bba38
Tim Xie <pdt.xie@gmail.com>
2023-12-08 15:41:44 -0800

    data

---
+++ data.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import pandas as pd
+from challenge_dataset import ChallengeDataset
+from torch.utils.data import DataLoader
+
+
+def load_pv():
+    # Assuming data already downloaded... see TODO some other file for this
+
+    # Load pv data by concatenating all data in this folder
+    # Can modify as needed to load specific data
+
+    data_dir = Path("/data/climatehack/official_dataset/pv/2020")
+    pv = pd.concat(
+        pd.read_parquet(parquet_file).drop("generation_wh", axis=1) for parquet_file in data_dir.glob('*.parquet')
+    )
+
+    return pv
+
+
+def load_data(dataset_type: str = "hrv", year: int = 2021):
+    # Once again, this is opening multiple datasets at once
+    # hrv = xr.open_dataset("data/satellite-hrv/2020/7.zarr.zip", engine="zarr", chunks="auto")
+    # opens a single dataset
+
+    data = xr.open_mfdataset(f"/data/climatehack/official_dataset/{dataset}/{year}/*.zarr.zip", engine="zarr", chunks="auto")
+
+    return data
+
+
+def create_dataset(dataset_type: str, year: int, site_locations, batch_size: int):
+    assert dataset_type in ("aerosols", "hrv", "nonhrv", "pv", "weather"), "ERROR LOADING DATA: dataset type provided is not correct [not of 'aerosols', 'hrv', 'nonhrv', 'pv' or 'weather']"
+    assert year == 2020 or year == 2021, "ERROR LOADING DATA: year provided not correct [not 2020 or 2021]"
+
+    pv, data = load_pv(), load_data(dataset, year)
+
+    dataset = ChallengeDataset(pv, data, site_locations=site_locations)
+    dataloader = DataLoader(dataset, batch_size=batch_size, pin_memory=True)
+
+    return dataloader
--- train_model.py
+++ train_model.py
@@ -1,38 +1,20 @@
 import os
 import matplotlib.pyplot as plt
-import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import xarray as xr
 
 from ocf_blosc2 import Blosc2
-from torch.utils.data import DataLoader
 from torchinfo import summary
 import json
-from pathlib import Path
 
+from data import create_dataset
 from submission.model import Model
-from challenge_dataset import ChallengeDataset
 
 plt.rcParams["figure.figsize"] = (20, 12)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Assuming data already downloaded... see TODO some other file for this
-
-# Load pv data by concatenating all data in this folder
-# Can modify as needed to load specific data
-data_dir = Path("/data/climatehack/official_dataset/pv/2020")
-pv = pd.concat(
-    pd.read_parquet(parquet_file).drop("generation_wh", axis=1)
-    for parquet_file in data_dir.glob('*.parquet')
-)
-
-# Once again, this is opening multiple datasets at once
-# hrv = xr.open_dataset("data/satellite-hrv/2020/7.zarr.zip", engine="zarr", chunks="auto")
-# opens a single dataset
-hrv = xr.open_mfdataset("/data/climatehack/official_dataset/nonhrv/2020/*.zarr.zip", engine="zarr", chunks="auto")
 
 # pre-computed indices corresponding to each solar PV site stored in indices.json
 with open("indices.json") as f:
@@ -40,20 +22,16 @@ with open("indices.json") as f:
         data_source: {
             int(site): (int(location[0]), int(location[1]))
             for site, location in locations.items()
-        }
-        for data_source, locations in json.load(f).items()
+        } for data_source, locations in json.load(f).items()
     }
+
+dataloader = create_dataset("hrv", 2021, site_locations, 32)
 
 summary(Model(), input_size=[(1, 12), (1, 12, 128, 128)])
 
 # Actually do the training wow
-BATCH_SIZE = 32
-
-dataset = ChallengeDataset(pv, hrv, site_locations=site_locations)
-dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, pin_memory=True)
-
 model = Model().to(device)
-criterion = nn.L1Loss()
+criterion = nn.MSELoss()
 optimiser = optim.Adam(model.parameters(), lr=1e-3)
 
 EPOCHS = 1
