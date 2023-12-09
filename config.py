6558793057eb32fa0c6d661f25a665d9a5da29f6
Val Rotan <valeriy.rotan@berkeley.edu>
2023-12-09 00:05:18 +0000

    useless config

---
+++ config.py
@@ -0,0 +1,20 @@
+from easydict import EasyDict as edict
+from datetime import datetime
+
+config = edict()
+
+# TRAIN CONFIG
+config.train = edict()
+config.train.batch_size = 32
+config.train.num_epochs = 1
+config.train.num_workers = 8
+config.train.lr = 1e-3
+
+# DATA CONFIG
+config.data = edict()
+config.data.pv_path = "/data/climatehack/official_dataset/pv"
+config.data.hrv_path = "/data/climatehack/official_dataset/hrv"
+config.data.nonhrv_path = "/data/climatehack/official_dataset/nonhrv"
+config.data.start_date = datetime(2020, 7, 1)
+config.data.end_date = datetime(2020, 7, 30)
+
