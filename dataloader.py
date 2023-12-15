b19fbf23897ab9123910289a17ce7067aca696ec
apgoldberg1 <apgoldberg@gmail.com>
2023-12-15 04:32:41 +0000

    dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,40 +1,60 @@
-import torch
+from torch.utils.data import Dataset
+import xarray as xr
+from ocf_blosc2 import Blosc2
+from functools import reduce
+
+months_num = 2
 class NonHrvDataset(Dataset):
     """Face Landmarks dataset."""
 
     def __init__(self, data_path="/data/climatehack/official_dataset/nonhrv"):
         self.data_path = data_path
-        self.index_map = {}
-        self.month_map = {0: 0}
+        self.index_map = [] #list of true indexes so index_map[0] = 7 would mean when we get(0) we actually get 7
+        self.month_map = []
+        self.month_lens = []
 
         def xr_open(path):
-            return xr.open(
+            return xr.open_dataset(
                     path,
                     engine="zarr",
                     consolidated=True,
                     )
 
-        def verify(real_idx):
-            return True
 
         year = 2021
-        self.data_2021 = [xr_open(f"/data/climatehack/official_dataset/nonhrv/{year}/{month}.zarr.zip") for month in range(1,13)]
+        self.data_2021 = [xr_open(f"/data/climatehack/official_dataset/nonhrv/{year}/{month}.zarr.zip") for month in range(1, months_num + 1)]
+
+        #self.data_len = sum([x.shape[0] for x in self.data_2021])
+
+        def verify(month, real_idx):
+            to_check = self.data_2021[month]["time"][real_idx:real_idx + 12]
+            return len(to_check) == 12 and reduce(lambda a, b: a and (b - a).item() == 300000000000, to_check) 
+        
+        next_real_index = 0
+        for cur_month in range(0, months_num):
+
+            print("on month ", cur_month)
+
+            month_idx = 0
+            month_len = self.data_2021[month_idx]["time"].shape[0]
+            while month_idx < month_len:
+                if verify(cur_month, month_idx):
+                    self.index_map.append(sum(self.month_lens) + next_real_index)
+                    next_real_index += 1
+                month_idx += 1
+
+            self.month_lens.append(month_len)
+
+        self.data_len = next_real_index
+
+
+            
 
     def __len__(self):
-        return sum([x.shape[0] for x in self.data_2021])
+        return self.data_len
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks], dtype=float).reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
+        idx = self.index_map[idx]
+        for tup in self.month_map:
+            if idx >= tup[1]:
+                return self.data_2021[tup[0]][idx - tup[1]]
