7eba57bbe40199c4584c214fef5fc7eff65b51a2
apgoldberg1 <apgoldberg@gmail.com>
2023-12-24 05:30:48 +0000

    small run adjustment

--- submission/run.py
+++ submission/run.py
@@ -17,7 +17,7 @@ class Evaluator(BaseEvaluator):
         """Sets up anything required for evaluation, e.g. loading a model."""
 
         self.model = Model().to(device)
-        self.model.load_state_dict(torch.load("model_deep_weather.pt", map_location=device))
+        self.model.load_state_dict(torch.load("model.pt", map_location=device))
         self.model.eval()
 
     def predict(self, features: h5py.File):
