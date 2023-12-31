81fc09c3cf443078be7ff97760b8bf3682276760
apgoldberg1 <apgoldberg@gmail.com>
2023-12-31 07:26:21 +0000

    watch out for occasional slow eval :(

--- submission/config.py
+++ submission/config.py
@@ -5,7 +5,7 @@ config = edict()
 
 # TRAIN CONFIG
 config.train = edict()
-config.train.model_save_name = "bigger_batch.pt"
+config.train.model_save_name = "noimage.pt"
 config.train.batch_size = 256
 config.train.num_epochs = 1000
 config.train.lr = 1e-3
