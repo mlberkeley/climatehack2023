6f39883c5d743425032c42ca1ad373a26969d4cb
Grace Tang <grace.tang@berkeley.edu>
2024-02-26 08:50:35 +0000

    Change to take in dictionary config

--- submission/models/build.py
+++ submission/models/build.py
@@ -1,23 +1,23 @@
-from models import ResNetPV, NonHRVMeta, MetaAndPv, MetaAndPv5, MainModel2
-from vit import VIT_Model
+from .models import ResNetPV, NonHRVMeta, MetaAndPv, MetaAndPv5, MainModel2
+from .vit import VIT_Model
 
+# Note that config is now a regular dictionary, NOT an easydict
 def build_model(config):
-    "Model builder."
-
-    model_type = config.model.name
+    model_type = config['model']['name']
+    model_config = config['model']['config']
 
     if model_type == 'resnetpv':
-        model = ResNetPV(config.model.config)
+        model = ResNetPV(model_config)
     elif model_type == 'nonhrvmeta':
-        model = NonHRVMeta(config.model.config)
+        model = NonHRVMeta(model_config)
     elif model_type == 'metaandpv':
-        model = MetaAndPv(config.model.config)
+        model = MetaAndPv(model_config)
     elif model_type == 'metaandpv5':
-        model = MetaAndPv5(config.model.config)
+        model = MetaAndPv5(model_config)
     elif model_type == 'mainmodel2':
-        model = MainModel2(config.model.config)
+        model = MainModel2(model_config)
     elif model_type == 'vit':
-        model = VIT_Model(config.model.config)
+        model = VIT_Model(model_config)
     else:
         raise NotImplementedError(f"Unknown model: {model_type}")
     
