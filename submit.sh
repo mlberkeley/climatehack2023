db5d08f5785bd94a33513f259f088b027ce2f367
Grace Tang <48929887+gractang@users.noreply.github.com>
2024-02-26 16:26:58 -0800

    Update submit.sh with model
    Co-authored-by: Val Rotan <valeriy.rotan@berkeley.edu>

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 # $1 = path to the model folder, should be something like
 #       ckpts/model_name
 # maybe cp the config as well and then mparse the config in run.py to figure out what model to import
-cp ckpts/$1/$1.pt.best_ema submission/model.pt
+cp ckpts/$1/model.pt.best_ema submission/model.pt
 cp ckpts/$1/config.json submission/config.json
 
 python3 -m doxa_cli login
